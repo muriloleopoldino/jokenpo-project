037627075ecb7105f2e2d967e189904a11bcdda7
muriloleopoldino <muriloleopoldino07@gmail.com>
2025-11-13 13:04:21 -0300

    Corrigindo erro

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const result = document.querySelector(".result");
-const humanScore = document.querySelector("#human-score")
-const machineScore = document.querySelector("#machine-score")
+const humanScore = document.querySelector("#human-score");
+const machineScore = document.querySelector("#machine-score");
 
-let humanScoreNumber = 0
-let machineScoreNumber = 0
-
-//ENUNS
-//SERVEM PARA QUE NÃO OCORRA A CHANCE DE TER ALGUM ERRO DE CARACTERES
+let humanScoreNumber = 0;
+let machineScoreNumber = 0;
 
 const GAME_OPTIONS = {
-    ROCK: 'rock',
-    PAPER: 'paper',
-    SCISSORS: 'scissors'
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSORS: "scissors",
 };
 
 function playHuman(humanChoice) {
@@ -21,26 +18,25 @@ function playHuman(humanChoice) {
 function playMachine() {
   const choices = ["rock", "paper", "scissors"];
   const randomNumber = Math.floor(Math.random() * 3);
-
   return choices[randomNumber];
 }
 
 function playTheGame(human, machine) {
-  console.log("Humano " + human + "Máquina " + machine);
+  console.log("Humano: " + human + " | Máquina: " + machine);
 
   if (human === machine) {
-    result.innerHTML = "Deu empate! 🤷‍♂️";
+    result.innerHTML = "Deu empate! ✍️";
   } else if (
     (human === "paper" && machine === "rock") ||
     (human === "rock" && machine === "scissors") ||
     (human === "scissors" && machine === "paper")
   ) {
-    humanScoreNumber++
-    humanScoreNumber.innerHTML = humanScoreNumber
+    humanScoreNumber++;
+    humanScore.innerHTML = humanScoreNumber; // ✅ CORREÇÃO AQUI
     result.innerHTML = "Você ganhou! 😎";
   } else {
-    machineScoreNumber++
-    machineScore.innerHTML = machineScoreNumber
-    result.innerHTML = "Você perdeu! 😭";
+    machineScoreNumber++;
+    machineScore.innerHTML = machineScoreNumber;
+    result.innerHTML = "Você perdeu! 😥";
   }
 }
